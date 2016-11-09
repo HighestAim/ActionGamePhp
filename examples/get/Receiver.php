301e79e8e788900f27baefde1d1ef667c55d2c4c
hwhovo <hwhovo@gmail.com>
2016-11-09 21:03:33 +0400

    get data and echo json file
    get login and password , encoding and echo encoded array. for using you
    must go url and add at last "?login=value1&password=value2" string

--- examples/get/Receiver.php
+++ examples/get/Receiver.php
@@ -1,5 +1,12 @@
 <?
-$name = $_GET["login"]; 
-$pwd = $_GET["pwd"];
-echo $name ; echo  "\n"; echo $pwd;
+$login = $_GET["login"]; 
+$password = $_GET["password"];
+
+$jsonLogin = json_encode($login);
+$jsonPassword = json_encode($password);
+
+
+$decodedLogin = json_decode($jsonLogin);
+$decodedPassword = json_decode($jsonPassword);
+echo $decodedLogin ; echo  "\n"; echo $decodedPassword;
 ?>
--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-﻿<?php include"php/example.php" ?>
+﻿<?php
+$login = $_GET["login"]; 
+$password = $_GET["password"];
+
+$arr = array("login"=>$login, "password"=>$password);
+
+    $jsonArray = json_encode($arr);
+
+    echo $jsonArray;
+?>
 
